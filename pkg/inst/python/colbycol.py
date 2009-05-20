30ab3987068e4037bcdce063bfe55fb7cd1cfd79
cjgb <cjgb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-05-20 23:00:36 +0000

    First submission
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colbycol@2 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/inst/python/colbycol.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import os
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    os.chdir( argv[1] )
+
+    conf_data = open( ".delete", "r" ).readlines()
+
+    file_in   = open( conf_data[0].split("|")[1].strip(), "r" )
+    sep       = conf_data[1].split(":")[1].strip()
+
+    skip      = int( conf_data[2].split(":")[1].strip() )
+
+    files_out = [ open( x.strip(), "w") for x in conf_data[3].split(":")[1].split(";") ]
+
+    cont = 0
+
+    for line in file_in:
+        cont += 1
+        if cont <= skip:
+            continue
+
+        line = unicode( line, errors = "ignore" )
+
+        tokens = line.split( sep )
+
+        if len( tokens ) != len( files_out ):
+            pass
+
+        for i in range( len( tokens ) ):
+            token = tokens[i].strip()
+            if token == "":
+                token = "NA"
+            files_out[i].write( token.encode("utf8") + "\n" )
+
+    for file_out in files_out:
+        file_out.close()
+
+    file_in.close()
+            
+if __name__ == "__main__":
+    sys.exit(main())
+
