b87aedf73994fb3dea418dd42a3996bf555f16c6
cjgb <cjgb@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-07-07 00:20:23 +0000

    Changed webpage.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/colbycol@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,42 +1,48 @@
+<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 
-<!-- This is the project specific website template -->
-<!-- It can be changed as liked or replaced by other content -->
-
-<?php
-
-$domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
-$group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
-$themeroot='http://r-forge.r-project.org/themes/rforge/';
-
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
-<!DOCTYPE html
-	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   ">
-
-  <head>
+<head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
-  </head>
+	<title>R Package ColbyCol</title>
+	<link href="http://r-forge.r-project.org/themes/rforge/styles/estilo1.css" rel="stylesheet" type="text/css" />
+</head>
 
 <body>
 
 <!-- R-Forge Logo -->
 <table border="0" width="100%" cellspacing="0" cellpadding="0">
 <tr><td>
-<a href="/"><img src="<?php echo $themeroot; ?>/images/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
+<a href="/"><img src="http://r-forge.r-project.org/themes/rforge/imagesrf/logo.png" border="0" alt="R-Forge Logo" /> </a> </td> </tr>
 </table>
 
 
-<!-- get project title  -->
-<!-- own website starts here, the following may be changed as you like -->
+<h2>ColByCol R package</h2>
 
-<p>This is a package intended for reading huge text data files into R by processing columns one by one individually.</p>
+<p>This package is intended for reading big datasets into R. It can be useful under certain conditions.</p>
 
+<h3>Reading big datasets into R</h3>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p>By default, R reads data into memory. It creates some problems when dealing with large datasets. Many solutions have been proposed for dealing with such datasets. Some of them can be found <a href="http://cran.r-project.org/web/views/HighPerformanceComputing.html">here</a>. Uploading your data into a DBMS can also be a solution.</p>
 
+<p>However, read.table function remains the main data import function in R. This function is memory inefficient and, according to some estimates, it requires three times as much memory as the size of a dataset in order to read it into R.</p>
+
+<p>The reason for such inefficiency is that R stores data.frames in memory as columns (a data.frame is no more than a list of equal length vectors) whereas text files consist of rows of records. Therefore, R's read.table needs to read whole lines, process them individually breaking into tokens and transposing these tokens into column oriented data structures.</p>
+
+<h3>ColByCol approach</h3>
+
+<p>ColByCol approach is memory efficient. Using Java code, tt reads the input text file and outputs it into several text files, each holding an individual column of the original dataset. Then, these files are read individually into R thus avoiding R's memory bottleneck.</p>
+
+<p>The approach works best for big files divided into many columns, specially when these columns can be transformed into memory efficient types and data structures: R representation of numbers (in some cases), and character vectors with repeated levels via factors occupy much less space than their character representation.</p>
+
+<p>Package ColByCol has been successfully used to read multi-GB datasets on a 2GB laptop.</p>
+
+<h3>Further package info</h3>
+
+<p>You can find it in <a href="http://cran.r-project.org/web/packages/colbycol/index.html">CRAN</a> and can be installed using R's install.package function.</p>
+
+<p>The project summary page can be found <a href="http://r-forge.r-project.org/projects/colbycol/">here</a>. You may also be interested in its page at <a href="http://crantastic.org/packages/colbycol">crantastic</a>.</p>
+
+<p>The package has been developed by <a href="http://www.datanalytics.com">Carlos J. Gil Bellosta</a>.</p>
 </body>
 </html>
+
